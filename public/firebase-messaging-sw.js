4e3bd6dd14bf65ee98d87b604829bce418b1bb91
zh <50803056+hhzule@users.noreply.github.com>
2020-08-05 13:24:18 +0500

    Create firebase-messaging-sw.js

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,16 @@
+importScripts('https://www.gstatic.com/firebasejs/7.17.1/firebase-app.js');
+ importScripts('https://www.gstatic.com/firebasejs/7.17.1/firebase-messaging.js');
+
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyAzzg7rSDdzvDimdI7HKYYYjxr-_vRaeSw",
+    authDomain: "expence-tracker-app.firebaseapp.com",
+    databaseURL: "https://expence-tracker-app.firebaseio.com",
+    projectId: "expence-tracker-app",
+    storageBucket: "expence-tracker-app.appspot.com",
+    messagingSenderId: "823965271012",
+    appId: "1:823965271012:web:2e0c987b910b1c551c3df1"
+  };
+  
+  firebaseConfig.initializeApp(firebaseConfig);
+   firebase.messaging();
